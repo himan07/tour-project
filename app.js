d483b0f1e790f61eba2843f17c625119361c25ca
Himanshu.Yadav <himanshuyadav@IND-DEL-MX-M208s-MacBook-Pro.local>
2024-09-24 12:55:31 +0530

    changes

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const tour = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tour-simple.json`)
 );
 
+// Get api call
 app.get("/api/v1/tour", (req, res) => {
   res.status(200).json({
     status: "success",
@@ -21,6 +22,25 @@ app.get("/api/v1/tour", (req, res) => {
   });
 });
 
+// Target data by ID
+app.get("/api/v1/tour/:id", (req, res) => {
+  const id = req.params.id * 1;
+  if (id > tour.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid Id",
+    });
+  }
+  const tours = tour.find((el) => el.id === id);
+  res.status(200).json({
+    message: "success",
+    data: {
+      tours,
+    },
+  });
+});
+
+// Post api call
 app.post("/api/v1/tour", (req, res) => {
   const newId = tour[tour.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
