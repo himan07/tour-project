22eb25693b4f5ca1899ab41679b0f66e31126b4b
Himanshu.Yadav <himanshuyadav@IND-DEL-MX-M208s-MacBook-Pro.local>
2024-09-24 12:30:47 +0530

    get and post api completed

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const fs = require("fs");
 
 const app = express();
 
+app.use(express.json()); //middleware
+
 const port = 3000;
 
 const tour = JSON.parse(
@@ -17,6 +19,24 @@ app.get("/api/v1/tour", (req, res) => {
       tours: tour,
     },
   });
+});
+
+app.post("/api/v1/tour", (req, res) => {
+  const newId = tour[tour.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tour.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tour-simple.json`,
+    JSON.stringify(tour),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 app.listen(port, () => {
