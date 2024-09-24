265170bdd89f0ff3f0e760def695ae150808c7cd
Himanshu.Yadav <himanshuyadav@IND-DEL-MX-M208s-MacBook-Pro.local>
2024-09-24 21:45:05 +0530

    changes

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const fs = require("fs");
+const morgan = require("morgan");
 
 const app = express();
 
-app.use(express.json()); //middleware
+// app.use(express.json()); //middleware
 
+// write your own middleware
+app.use((req, res, next) => {
+  console.log("Hello from the middleware!");
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// using 3rd party middleware
+app.use(morgan("dev"));
 const port = 3000;
 
 const tour = JSON.parse(
@@ -15,6 +29,7 @@ const tour = JSON.parse(
 app.get("/api/v1/tour", (req, res) => {
   res.status(200).json({
     status: "success",
+    requestTime: req.requestTime,
     results: tour.length,
     data: {
       tours: tour,
@@ -58,6 +73,45 @@ app.post("/api/v1/tour", (req, res) => {
     }
   );
 });
+
+// update data using patch method
+app.patch("/api/v1/tour/:id", (req, res) => {
+  const id = req.params.id * 1;
+  if (id > tour.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid Id",
+    });
+  }
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour: "<Updated tour is here!>",
+    },
+  });
+});
+
+// delete api call
+app.delete("/api/v1/tour/:id", (req, res) => {
+  const id = req.params.id * 1;
+  if (id > tour.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid Id",
+    });
+  }
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+});
+
+// param middleware
+app.param('id', (req, res, next, value)=>{
+console.log(`Tour id is: ${value}`)
+next()
+
+})
 
 app.listen(port, () => {
   console.log(`app is listening on the port: ${port}`);
